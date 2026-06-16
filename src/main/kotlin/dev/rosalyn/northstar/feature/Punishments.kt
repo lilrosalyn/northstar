9ed118b60d86099954697b52f89fded8559d8761
lilrosalyn <untitledgeese@gmail.com>
2026-06-16 18:58:51 -0500

    change to modal for paragraph reasons

--- src/main/kotlin/dev/rosalyn/northstar/feature/Punishments.kt
+++ src/main/kotlin/dev/rosalyn/northstar/feature/Punishments.kt
@@ -5,6 +5,7 @@ package dev.rosalyn.northstar.feature
 import dev.rosalyn.northstar.ModuleSettings
 import dev.rosalyn.northstar.config.MessageConfig
 import dev.rosalyn.northstar.event.GuildInitializeEvent
+import dev.rosalyn.northstar.lib.component.makeModal
 import dev.rosalyn.northstar.lib.tryRetrieveMember
 import dev.rosalyn.northstar.logger
 import dev.rosalyn.northstar.schema.getGuild
@@ -12,6 +13,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.components.textinput.TextInputStyle
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.exceptions.ContextException
 import net.dv8tion.jda.api.exceptions.ErrorResponseException
@@ -33,7 +35,6 @@ private fun initializeModule(event: GuildInitializeEvent): List<CommandData> {
     return listOf(
         Commands.slash("ban", "Ban a user.")
             .addOption(OptionType.USER, "user", "The user to ban.", true)
-            .addOption(OptionType.STRING, "reason", "The punishment reason.", true)
             .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MODERATE_MEMBERS))
     )
 }
@@ -56,28 +57,34 @@ private suspend fun onExecute(event: SlashCommandInteractionEvent) = withContext
 
     val moderator = interaction.user
     val user = interaction.getOption("user")!!.asUser
-    val reason = interaction.getOption("reason")!!.asString
-    val member = guild.tryRetrieveMember(user)
-    var sentMessage = false
 
-    if (member != null) {
-        try {
-            val channel = user.openPrivateChannel().complete()
-            val messageData = config.message!!.toMessage("reason" to reason)
-            channel.sendMessage(messageData).complete()
-            sentMessage = true
-        } catch (_: Exception) {}
-    }
+    interaction.replyModal(makeModal("Punish User", onSubmit = {
+        val reason = getValue("reason")!!.asString
 
-    guild.ban(user, 7, TimeUnit.DAYS)
-        .reason(reason + "\nBanned by ${moderator.name} (${moderator.id})")
-        .queue({
-            interaction.reply("${user.name} has been banned." + if (sentMessage) "" else "\nThe DM failed to send.")
-                .setEphemeral(true)
-                .queue()
-        }, {
-            interaction.reply("We failed to ban ${user.name}. Is the bot's role above the member's roles?")
-                .setEphemeral(true)
-                .queue()
-        })
+        val member = guild.tryRetrieveMember(user)
+        var sentMessage = false
+
+        if (member != null) {
+            try {
+                val channel = user.openPrivateChannel().complete()
+                val messageData = config.message!!.toMessage("reason" to reason)
+                channel.sendMessage(messageData).complete()
+                sentMessage = true
+            } catch (_: Exception) {}
+        }
+
+        guild.ban(user, 7, TimeUnit.DAYS)
+            .reason(reason + "\nBanned by ${moderator.name} (${moderator.id})")
+            .queue({
+                interaction.reply("${user.name} has been banned." + if (sentMessage) "" else "\nThe DM failed to send.")
+                    .setEphemeral(true)
+                    .queue()
+            }, {
+                interaction.reply("We failed to ban ${user.name}. Is the bot's role above the member's roles?")
+                    .setEphemeral(true)
+                    .queue()
+            })
+    }) {
+        textInput("reason", TextInputStyle.PARAGRAPH, "Reason")
+    }).queue()
 }
